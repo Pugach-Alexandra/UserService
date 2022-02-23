01cef9ef7d0bc1e9e91e08c4f22ef15158cf4462
Pugach-Alexandra <alexa.pugach24@gmail.com>
2022-02-23 17:35:47 +0200

    Changes for heroku

--- src/main/java/com/example/Mafia/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Mafia.repository;
-
-import com.example.Mafia.model.User;
-
-import org.springframework.data.cassandra.repository.CassandraRepository;
-
-public interface UserRepository extends CassandraRepository<User,Long> {
-
-}
