bd536ccc1468a549175e5d18af25c017d0106304
Pugach-Alexandra <alexa.pugach24@gmail.com>
2022-03-18 12:32:59 +0200

    Logger added

--- src/main/java/com/example/Mafia/MafiaApplication.java
+++ src/main/java/com/example/Mafia/MafiaApplication.java
@@ -1,10 +1,13 @@
 package com.example.Mafia;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import java.nio.file.Path;
 
@@ -14,7 +17,7 @@ public class
 MafiaApplication {
 
 	public static void main(String[] args) {
-
+		final Logger LOGGER = LoggerFactory.getLogger(MafiaApplication.class);
 		SpringApplication.run(MafiaApplication.class, args);
 
 	}
@@ -23,6 +26,12 @@ MafiaApplication {
 
 		Path bundle = astraProperties.getSecureConnectBundle().toPath();
 		return builder -> builder.withCloudSecureConnectBundle(bundle);
+
+	}
+	@Bean
+	public RestTemplate getRestTemplate() {
+
+		return new RestTemplate();
 
 	}
 }
